ba378c3d49656ad2716bc79aaae733916bd65e5b
y0534160317-bit <y0534160317@gmail.com>
2025-12-04 17:40:51 +0200

    after changing dbcontext

--- ToDoCs/ToDoDbContext.cs
+++ ToDoCs/ToDoDbContext.cs
@@ -22,7 +22,7 @@ public partial class ToDoDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;user=root;password=yonitshvartsmysql;database=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
+        => optionsBuilder.UseMySql("server=bwdhfhsxod0ovlwo6nzt-mysql.services.clever-cloud.com;user=uijd9nyojjlr1zie;password=mBQhsTwUFFLAJudjlUsK;database=bwdhfhsxod0ovlwo6nzt", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.44-mysql"));
 
    protected override void OnModelCreating(ModelBuilder modelBuilder)
 {
